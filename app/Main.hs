cafad707ce837961958ead702b2602276fedea3b
Gabriel Araujo <gabriel_scf@hotmail.com>
2021-12-18 23:09:06 -0300

    Allow CORS requests.

--- app/Main.hs
+++ app/Main.hs
@@ -11,8 +11,20 @@ module Main where
 import           Yesod
 import           Yesod.Static
 
+import           Network.Wai.Middleware.Cors
+import           Network.Wai.Handler.Warp (run)
+
 import           Frontend
 import           Storage
+
+allowCors = cors (const $ Just appCorsResourcePolicy)
+
+appCorsResourcePolicy :: CorsResourcePolicy
+appCorsResourcePolicy =
+    simpleCorsResourcePolicy
+        { corsMethods = ["OPTIONS", "GET", "PUT", "POST"]
+        , corsRequestHeaders = ["Authorization", "Content-Type"]
+        }
 
 main :: IO ()
 main = do
@@ -23,7 +35,10 @@ main = do
           ++ "."
           ++ spacer
   static@(Static settings) <- static "static/"
-  warp port $ App static
+
+  -- Convert the Yesod website into a WAI Application.
+  app     <- toWaiAppPlain $ App static
+  run port $ allowCors app
   where
-    port = 5000
+    port   = 5000
     spacer = take 4 $ repeat '\n'
